0183e09bb6e122fe7c4cceda801d555f975e0a76
rudinesurya <rudinesurya@gmail.com>
2025-05-10 03:22:45 +0100

    change to yarn

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -15,6 +15,10 @@ import { authContext } from './auth.context';
       useFactory: (configService: ConfigService) => ({
         server: {
           context: authContext,
+          cors: {
+            origin: ['http://localhost:8080'], // Add allowed origins here
+            credentials: true, // Allow cookies to be sent
+          },
         },
         gateway: {
           supergraphSdl: new IntrospectAndCompose({
